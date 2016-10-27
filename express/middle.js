07967e87724460ec7badac71ec9c325ba4fb7a59
godi13 <godi13@vip.qq.com>
2016-10-27 11:08:25 +0800

    middle&use

---
+++ express/middle.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var cb0 = function(req, res, next) {
+  console.log("CB0");
+  next();
+}
+
+var cb1 = function(req, res, next) {
+  console.log("CB1");
+  next();
+}
+
+// var cb2 = function(req, res, next) {
+//   res.send("Hello from C!");
+// }
+
+app.get('/example/d', [cb0, cb1], function(req, res, next) {
+  console.log("response will be sent by the next function ...");
+  next();
+}, function(req, res) {
+  res.send("Hello from D!");
+});
+
+app.listen(8081, function() {
+  console.log("接口已启动");
+});
---
+++ express/must.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+// app.get('*', function(req, res, next) {
+//   console.log("必经路由");
+//   next();
+// });
+app.use(function(req, res, next) {
+  console.log("必经路由");
+  next();
+})
+
+app.get('/index', function(req, res, next) {
+  res.send('index');
+});
+
+app.get('/go', function(req, res, next) {
+  res.send('go');
+});
+
+app.listen(8081, function() {
+  console.log("接口已启动");
+});
