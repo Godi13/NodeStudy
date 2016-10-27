05db976d435b650b1ab505c2915e1a2eab06d736
godi13 <godi13@vip.qq.com>
2016-10-27 17:53:46 +0800

    express

--- express/app.js
+++ express/app.js
@@ -1,19 +1,61 @@
+//第三方模块的加载
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({
-  extended: false
-})
-
+var mysql = require('mysql');
 app.use(express.static('public'));
-app.get('/index', function(req, res) {
-  res.sendFile(__dirname + "/views/" + "index.html");
-})
-app.post('/index', urlencodedParser, function(req, res) {
-  console.log(req.body.username);
-  res.redirect(`https://www.baidu.com/s?wd=${req.body.username}&rsv_spt=1&rsv_iqid=0xd8133afb00027f20&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=3&rsv_sug1=3&rsv_sug7=100&rsv_t=1c3db%2Fd6%2Bg%2BxIrkjc1GSeBly9moEht3bfjGG3GkEkKfXCzVKCOir2X8MzES0HqLxEcDZ&rsv_sug2=0&inputT=3835&rsv_sug4=3836`);
-})
 
+// 配合Swig
+var swig = require('swig');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+//设置mysql
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'PHPtest'
+});
+connection.connect();
+
+// 配置路由
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: "Express 小程序"
+  });
+});
+app.get('/receive', function(req, res, next) {
+  console.log('前台发来的数据', req.query.username);
+  var post = {
+    username: req.query.username
+  };
+  var query = connection.query('INSERT INTO userinfo SET ?', post, function(err, result) {
+    if (err) {
+      res.json({
+        success: 'no',
+        msg: '插入失败'
+      });
+    } else {
+      res.json({
+        success: 'ok',
+        msg: '插入成功'
+      });
+    }
+  });
+});
+
+// 容错机制
+app.get('*', function(req, res, next) {
+  res.status(404);
+  res.end('404');
+});
+
+app.use(function(err, req, res, next) {
+  res.status(500);
+  res.end('error...');
+});
+
+// 启动服务
 app.listen(8081, function() {
   console.log("接口已启动");
 });
--- express/public/scripts/index.js
+++ express/public/scripts/index.js
@@ -1,6 +1,19 @@
-console.log("123");
-
-function test() {
-
-}
-test();
+$(function(argument){
+  $('#btnsubmit').click(function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/receive',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        username: $('#username').val()
+      },
+      success: function(data) {
+        console.info(data.msg);
+      },
+      error: function() {
+        alert('error');
+      }
+    });
+  })
+})
